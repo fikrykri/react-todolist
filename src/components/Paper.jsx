6400a83d4ab3f6618e653f5e2f94ea8001e3decd
FikryAS <azizfikry15@gmail.com>
2020-12-21 15:37:43 +0700

    formated document

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Paper = ({children}) => {
+const Paper = ({ children }) => {
   return (
     <div className="container">
-      <div className="frame">
-        {children}
-      </div>
+      <div className="frame">{children}</div>
     </div>
   );
-}
+};
 
 Paper.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-}
+    PropTypes.node,
+  ]),
+};
 
 export default Paper;
