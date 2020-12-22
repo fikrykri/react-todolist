b569e76898dc24cf135568fa563c61237287cce7
FikryAS <azizfikry15@gmail.com>
2020-12-22 14:16:55 +0700

    create styling button with css module and classname library

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+/**
+ * note untuk classnames harus diinstal lewat terminal klo offline
+ * menggunakan vscode "npm install classnames --save"
+ * dependenci classnames ini berfungi untuk menampung classnames/library classnames
+ */
+
+import styles from "./button.module.css";
+
+const Button = ({ text, onClick, color, align }) => {
+  // code dibawah tanpa menggunakan css module dan cssnya terdapat diglobal css
+  // const classNames = [
+  //   "header-btn",
+  //   color === "black" && "main-black-color",
+  //   color === "red" && "main-red-color",
+  //   align === "left" && "align-left",
+  //   align === "right" && "align-right"
+  // ].join(" ");
+  
+  // Penggunaan css module dari react
+  const classNames = cx(styles.headerBtn, {
+    [styles.mainBlackColor]: color === "black",
+    [styles.mainRedColor]: color === "red",
+    [styles.alignLeft]: align === "left",
+    [styles.alignRight]: align === "right"
+  });
+  /**
+ * join diatas memberikan spasi antar index/item pada array agar 
+ * komponen pada array bisa dijalankan oleh classname
+ */
+  return (
+    <button className={classNames} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps ={
+  text: 'Button',
+  color: 'black',
+  align: 'center'
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  color: PropTypes.oneOf(['black', 'red']),
+  align: PropTypes.oneOf(['left', 'right'])
+};
+
+export default Button;
